d60a2c28891f156ad7a23aeefcc081897cbd53b0
mike <mwmakarov@bk.ru>
2024-04-02 23:37:04 +0400

    nginx reverse proxy with docker and express/react

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import chalk from "chalk";
 import cookieParser from "cookie-parser";
-import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import { morganMiddleware, systemLogs } from "./utils/Logger.js";
+import { configDotenv } from "dotenv";
+import path from "path";
+
+configDotenv({path: path.join(process.cwd(), "..", ".env")})
 
 const app = express();
 
@@ -14,14 +17,13 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-
-app.use(morganMiddleware)
+app.use(morganMiddleware);
 
 app.get("/api/v1/test", (req, res) => {
     res.json({Hi: "Welcome!"});
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`${chalk.green.bold("✔")} Server started on the port: ${chalk.yellow.bold(PORT)} in ${chalk.magenta.bold(process.env.NODE_ENV)} mode`);
---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div><h1>Welcome to infoice app!!!</h1></div>
+  )
+}
+
+export default App
