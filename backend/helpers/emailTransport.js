91903639a112a9308638df19e07ea3cdcd47a8f4
mike <mwmakarov@bk.ru>
2024-04-03 12:57:07 +0400

    emailTransport config

---
+++ backend/helpers/emailTransport.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import nodemailer from "nodemailer";
+import { configDotenv } from "dotenv";
+
+configDotenv({path: path.join(process.cwd(), "..", ".env")})
+
+let transporter;
+
+if (process.env.NODE_ENV === "development") {
+    transporter = nodemailer.createTransport({
+        host: "mailhog",
+        port:"1025"
+    })
+} else if (process.env.NODE_ENV === "production") {
+    transporter.nodemailer.createTransport({
+        
+    })
+}
+
+export default transporter;
