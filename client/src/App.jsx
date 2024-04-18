bb4f24eafbe72067e6dcf4206bc307578fcf0375
mike <mwmakarov@bk.ru>
2024-04-18 22:15:15 +0400

    created and inserted register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
-import { CssBaseline } from "@mui/material"
-import { ThemeProvider } from "@mui/material/styles"
-import { Route, Routes } from "react-router-dom"
-import {ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.min.css"
-import Footer from './components/Footer.jsx'
-import Layout from './components/Layout.jsx'
-import NotFound from "./components/NotFound.jsx"
-import { customTheme } from "./customTheme.js"
-import useTitle from './hooks/useTitle.jsx'
-import HomePage from "./pages/HomePage.jsx"
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Footer from "./components/Footer.jsx";
+import Layout from "./components/Layout.jsx";
+import NotFound from "./components/NotFound.jsx";
+import { customTheme } from "./customTheme.js";
+import useTitle from "./hooks/useTitle.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import RegisterPage from "./features/auth/pages/RegisterPage.jsx";
 
 const App = () => {
-  useTitle("Чековая книжка - Стартовая страница")
-  return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline/>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<HomePage/>}/>
+    useTitle("Чековая книжка - Стартовая страница");
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="register" element={<RegisterPage />} />
 
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+            <Footer />
+            <ToastContainer theme="dark" />
+        </ThemeProvider>
+    );
+};
 
-
-          <Route path="*" element={<NotFound/>}/>
-        </Route>
-      </Routes>
-      <Footer/>
-      <ToastContainer theme="dark"/>
-    </ThemeProvider>
-  )
-}
-
-export default App
+export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,28 +1,25 @@
 import { Box, Button, Grid, Link, styled, Typography } from "@mui/material";
 import { useNavigate, Link as RouterLink } from "react-router-dom";
-import '../styles/homepage.css';
+import "../styles/homepage.css";
 
-
-const StyledTypography = styled(Typography)(({theme}) => (
-    {
-        fontSize: "12rem",
-        [theme.breakpoints.down("sm")] : {
-            fontSize: "9rem"
-        }
-    }
-))
+const StyledTypography = styled(Typography)(({ theme }) => ({
+    fontSize: "12rem",
+    [theme.breakpoints.down("sm")]: {
+        fontSize: "9rem",
+    },
+}));
 
 const CreateAccountButton = styled(Button)({
     backgroundColor: "#006888",
-    borderColor:"#555 !important",
+    borderColor: "#555 !important",
     borderRadius: "25px",
     border: "3px solid",
     "&:hover": {
         borderColor: "#fff !important",
         boxShadow: "none",
-        backgroundColor:"#087fa4"
-    }
-})
+        backgroundColor: "#087fa4",
+    },
+});
 
 function HomePage() {
     const navigate = useNavigate();
@@ -30,34 +27,57 @@ function HomePage() {
         <header className="masthead main-bg-image">
             <Grid>
                 <Grid item md={12} lg={12} sm={6}>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column"
-                    }}>
-                        <StyledTypography 
-                            variant="h1" 
-                            align="center" 
-                            sx={{textTransform: "uppercase"}}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <StyledTypography
+                            variant="h1"
+                            align="center"
+                            sx={{
+                                textTransform: "uppercase",
+                                fontSize: "172px",
+                            }}
                             className="homepage-header"
                         >
                             Чековая Книжка
                         </StyledTypography>
-                        <Typography align="center" variant="h4" component="div" gutterBottom sx={{color: "rgba(255,255,255,0.6"}}>
-                            Не важно каким бизнесом Вы занимаетесь.<br/>Работа с чеками станет гораздо проще!
+                        <Typography
+                            align="center"
+                            variant="h4"
+                            component="div"
+                            gutterBottom
+                            sx={{ color: "rgba(255,255,255,0.6" }}
+                        >
+                            Не важно каким бизнесом Вы занимаетесь.
+                            <br />
+                            Работа с чеками станет гораздо проще!
                         </Typography>
                     </Box>
-                    <Box sx={{display:"flex", justifyContent:"center", marginTop:"10px"}}>
-                        <CreateAccountButton 
-                            variant="contained" 
-                            color="success" 
-                            size="large" 
-                            sx={{fontSize:"1.5em", borderRadius: '25px'}}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            marginTop: "10px",
+                        }}
+                    >
+                        <CreateAccountButton
+                            variant="contained"
+                            color="success"
+                            size="large"
+                            sx={{ fontSize: "1.5em", borderRadius: "25px" }}
                             // onClick={() => navigate("/register")}
                         >
-                            <Link 
-                                component={RouterLink} 
-                                to="/register" 
-                                sx={{textDecoration:"none", color: "#fff", fontSize: "2rem"}}
+                            <Link
+                                component={RouterLink}
+                                to="/register"
+                                sx={{
+                                    textDecoration: "none",
+                                    color: "#fff",
+                                    fontSize: "2rem",
+                                }}
                             >
                                 Создать профиль
                             </Link>
@@ -66,7 +86,7 @@ function HomePage() {
                 </Grid>
             </Grid>
         </header>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
